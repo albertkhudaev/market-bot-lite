f9ae8b9564fa9463addc7379580753298ba60535
Albert Khudaev <albert-khudaev@yandex.ru>
2021-04-16 22:00:41 +0500

    Добавил возможность запуска демона

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from data.config import dbsource
-if dbsource == "pg":
-    from utils.db_api.database import create_db
+import daemon
+from sys import argv
+
+status = argv
+
+def main_mainfunc():
+    from data.config import dbsource
+    if dbsource == "pg":
+        from utils.db_api.database import create_db
 
 
-async def on_startup(dp):
-    import filters
-    import middlewares
-    filters.setup(dp)
-    middlewares.setup(dp)
+    async def on_startup(dp):
+        import filters
+        import middlewares
+        filters.setup(dp)
+        middlewares.setup(dp)
 
-    from utils.notify_admins import on_startup_notify
-    await on_startup_notify(dp)
-    #await create_db()
+        from utils.notify_admins import on_startup_notify
+        await on_startup_notify(dp)
+        #await create_db()
 
-if __name__ == '__main__':
-    from aiogram import executor
-    from handlers import dp
+    if __name__ == '__main__':
+        from aiogram import executor
+        from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup)
+        executor.start_polling(dp, on_startup=on_startup)
+
+
+if status == "daemon":
+    with daemon.DaemonContext():
+        main_mainfunc()
+else:
+    main_mainfunc()
